f4a315c915b930afb68ae358138e1c3d5ec812bc
nazeersathak <nazeersathak@gmail.com>
2022-09-05 15:56:14 +0530

    express sessions part-2

---
+++ models/user.js
@@ -0,0 +1,21 @@
+// import mongoose from 'mongoose'
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var User = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("User", User);
