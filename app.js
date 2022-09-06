59dc99100042f38aa7eaf328331718ad5e61d07e
nazeersathak <nazeersathak@gmail.com>
2022-09-06 10:18:37 +0530

    passport session small bug

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+var authenticate = require("./authenticate");
+var passport = require("passport");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -49,23 +51,18 @@ app.use(
   })
 );
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use(passport.initialize());
+app.use(passport.session());
 
 function auth(req, res, next) {
-  console.log(req.session);
-  if (!req.session.user) {
+  console.log(req.User);
+
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
-    err.status = 401;
-    return next(err);
+    err.status = 403;
+    next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
