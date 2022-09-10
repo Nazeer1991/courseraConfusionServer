cf175e156b8569eb5cfbd786b082cc8f0aa32aa7
nazeersathak <nazeersathak@gmail.com>
2022-09-10 19:58:36 +0530

    updated the upload Router js

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,57 +1,57 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const multer = require("multer");
-// const authenticate = require("../authenticate");
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const authenticate = require("../authenticate");
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "public/images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-// const imageFileFilter = (req, file, cb) => {
-//   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-//     return cb(new Error("You can upload on ly image files"));
-//   }
-//   cb(null, true);
-// };
+const imageFileFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error("You can upload on ly image files"));
+  }
+  cb(null, true);
+};
 
-// const upload = multer({ storage: storage, fileFilter: image });
+const upload = multer({ storage: storage, fileFilter: image });
 
-// const uploadRouter = express.Router();
+const uploadRouter = express.Router();
 
-// uploadRouter.use(bodyParser.json());
+uploadRouter.use(bodyParser.json());
 
-// uploadRouter
-//   .route("/")
-//   .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end("GET operationnot supported on /imageUpload");
-//   })
-//   .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end("PUT operationnot supported on /imageUpload");
-//   })
-//   .post(
-//     authenticate.verifyUser,
-//     authenticate.verifyAdmin,
-//     upload.single("imageFile"),
-//     (req, res) => {
-//       res.statusCode = 200;
-//       res.setHeader("Content-Type", "application/json");
-//       res.json(req.file);
-//     }
-//   )
-//   .delete(
-//     authenticate.verifyUser,
-//     authenticate.verifyAdmin,
-//     (req, res, next) => {
-//       res.statusCode = 403;
-//       res.end("DELETE operationnot supported on /imageUpload");
-//     }
-//   );
+uploadRouter
+  .route("/")
+  .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("GET operationnot supported on /imageUpload");
+  })
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operationnot supported on /imageUpload");
+  })
+  .post(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    upload.single("imageFile"),
+    (req, res) => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json(req.file);
+    }
+  )
+  .delete(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end("DELETE operationnot supported on /imageUpload");
+    }
+  );
 
-// module.exports = uploadRouter;
+module.exports = uploadRouter;
